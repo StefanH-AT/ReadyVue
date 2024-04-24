16368638e45286773526f6cdef02ca18420d705c
Stefan Heinz <stefan.heinz.02@gmail.com>
2024-04-24 02:47:00 +0200

    Add TextInput component

---
+++ component/TextInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-input-shift-wrapper">
+    <input id="input" type="text" class="text-input" placeholder="" v-model="model" @keyup.enter="emit('submit')">
+    <label for="input">{{ label }}</label>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+export interface Props {
+  label?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  label: ""
+});
+const model = defineModel<string>();
+const emit = defineEmits<{
+  submit: [void]
+}>();
+
+</script>
